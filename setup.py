eeef7b8a272bde31fdc62872f81e2415b480a59d
Martin Ortbauer <mortbauer@gmail.com>
2013-09-13 15:26:10 +0200

    added a LICENSE

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name="configreader",
    description="An easy-to-use, powerful configuration module for Python",
    long_description = open('README.rst','r').read(),
-   license="""Copyright (C) 2004-2010 by Martin Ortabuer. All Rights Reserved.
+   license="""Copyright (C) 2013 by Martin Ortabuer. All Rights Reserved.
       See LICENSE for license.""",
     version = "0.0.1",
     author = "Martin Ortbauer",
