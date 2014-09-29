6656a1b3ed1cd4854de192a2d2f99b3833aa6fd0
Martin Ortbauer <mortbauer@gmail.com>
2014-09-29 13:22:26 +0200

    renamed the tests dir; added some classifiers

--- setup.py
+++ setup.py
@@ -3,11 +3,17 @@ from setuptools import setup
 setup(name="configreader",
     description="An easy-to-use, powerful configuration module for Python",
     long_description = open('README.rst','r').read(),
-    license="""Copyright (C) 2013 by Martin Ortabuer. All Rights Reserved.
-        See LICENSE for license.""",
-    version = "0.0.1",
+    license="""MIT""",
+    version = "0.0.2",
     author = "Martin Ortbauer",
     author_email = "mortbauer@gmail.com",
     url = "https://github.com/mortbauer/configreader",
     py_modules = ["configreader"],
+    classifiers=[
+          "Development Status :: Alpha",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python",
+          "Topic :: Utilities",
+    ],
     )
